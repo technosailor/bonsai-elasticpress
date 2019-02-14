0bea8fccbd13434e17fbd384a9dfc4f38f06d64a
Aaron Brazell <aaron@technosailor.com>
2019-02-14 17:17:51 -0500

    Init

---
+++ bonsai-elasticpress.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Bonsai Integration for ElasticPress
+Description: Adds necessary Authentication fields to allow ElasticPress to communicate with Bonsai hosted Elasticsearch
+Author:      Aaron Brazell
+Version:     1.0
+Author URI:  https://bonsai.io/
+*/
+
+define( 'BASE_PATH', plugin_dir_path( __FILE__ ) );
+require_once BASE_PATH . 'vendor/autoload.php';
+
+add_action( 'plugins_loaded', function () {
+	Init::instance()->init();
+} );
